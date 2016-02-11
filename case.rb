3f7ac229ab653d78159ef84ebf97d8462e903c36
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-11 17:51:59 -0600

    [add] case :heart_eyes:

---
+++ case.rb
@@ -0,0 +1,36 @@
+class HolaMundo
+	def initialize()
+		
+	end
+
+	def saluda()
+		edad = 2
+		
+		#case edad
+		#when 0..11 then
+		#	puts "Eres un nino"
+		#when 12..17 then
+		#	puts "Eres un joven"
+		#when 18..45 then
+		#	puts "Eres un adulto"
+		#when 46..60 then
+		#	puts "Adulto mayor"
+		#else
+		#	puts "Error en la variable"
+		#end
+
+		respuesta = case edad
+		when 0..11 then
+			"Eres un nino"
+		when 12..17 then
+			"Eres un joven"
+		else
+			"Error en la variable"
+		end
+
+		puts respuesta
+
+	end
+end
+objeto = HolaMundo.new()
+objeto.saluda
