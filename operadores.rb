60147e13fd781d5590f25f1a3c4082d748b3aac6
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-11 02:45:13 -0600

    [add] operators

---
+++ operadores.rb
@@ -0,0 +1,26 @@
+class HolaMundo
+	def initialize()
+		
+	end
+
+	def saluda()
+		suma 			= 2 + 3
+		resta 			= 2 - 3
+		multiplicacion 	= 2 * 3
+		division 		= 2/3		
+		exponente 		= 2**3
+
+		variable = (10+3)/(3*2) 
+
+		puts variable
+		puts suma
+		puts resta
+		puts multiplicacion
+		puts division
+		puts exponente
+	end
+
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
