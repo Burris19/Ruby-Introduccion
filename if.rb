5e08eb41f5b9df79caea3c1c9eb8137060c040ee
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-11 03:05:48 -0600

    [update] if operators

---
+++ if.rb
@@ -0,0 +1,18 @@
+class HolaMundo
+	def initialize()
+		
+	end
+
+	def saluda()
+		hora = 14
+		if hora < 12
+			puts "Buenos dias"
+		else			
+			puts "Buenas tardes"
+		end
+
+	end
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
