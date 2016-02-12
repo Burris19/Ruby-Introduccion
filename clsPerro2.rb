7819ece5b73bca50c07b928347e659ed0a42a412
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-12 00:49:26 -0600

    [add] construct

---
+++ clsPerro2.rb
@@ -0,0 +1,18 @@
+class Perro
+	def initialize(nombre = 'sin nombre', raza = 'sin raza')
+		@nombre = nombre
+		@raza 	= raza
+	end
+
+	def ladrar
+		return "gua gua"
+	end
+
+	def dame_nombre
+		return @nombre
+	end
+
+	def dame_raza
+		return @raza
+	end
+end
---
+++ main2.rb
@@ -0,0 +1,11 @@
+require_relative 'clsPerro2.rb'
+
+dog = Perro.new('Firulais', 'Chihuahua')
+dogThow = Perro.new('Boby')
+
+puts dog.dame_raza
+puts dog.dame_nombre
+
+
+puts dogThow.dame_nombre
+puts dogThow.dame_raza
